d377bc4611b5b25db12f29034182e87c52ad2768
AzizbekGayratov <azizbekgairatov@gmail.com>
2024-08-10 16:07:10 +0500

    chunks added

--- src/components/CarouselSection/CarouselComponent.jsx
+++ src/components/CarouselSection/CarouselComponent.jsx
@@ -6,33 +6,47 @@ import React from "react";
 
 export default function CarouselComponent({ items }) {
   const router = useRouter();
+
+  const chunkArray = (items, chunkSize) => {
+    const chunkedArr = [];
+    for (let i = 0; i < items.length; i += chunkSize) {
+      const chunk = items.slice(i, i + chunkSize);
+      chunkedArr.push(chunk);
+    }
+    return chunkedArr;
+  };
+
+  const chunkedItems = chunkArray(items, 4);
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel slideInterval={800}>
-        {items.map((item) => (
+        {chunkedItems.map((chunk, index) => (
           <div
-            key={item.cca2}
-            className="flex  h-full items-center justify-center bg-gray-400 bg-opacity-20 dark:bg-gray-700 dark:text-white"
-            onClick={() => router.push(`/${item.cca2}`)}
+            key={index}
+            className="grid grid-cols-4 h-full  items-center justify-center bg-gray-400 bg-opacity-20 dark:bg-gray-700 dark:text-white"
           >
-            <div className="flex gap-20 items-center justify-center">
-              <Image
-                src={item.flags.svg}
-                width={300}
-                height={300}
-                alt={item.name.common}
-              />
-              <div>
-                <p className="text-4xl font-bold mb-4">{item.name.common}</p>
-                <p className="text-2xl">
-                  {String(item.population).replace(
-                    /\B(?=(\d{3})+(?!\d))/g,
-                    ","
-                  )}
-                  <span className="text-xl"> people</span>
-                </p>
+            {chunk.map((item) => (
+              <div key={item.cca2} onClick={() => router.push(`/${item.cca2}`)}>
+                <div className="flex flex-col items-center justify-center">
+                  <Image
+                    src={item.flags.svg}
+                    width={80}
+                    height={80}
+                    alt={item.name.common}
+                  />
+                  <div>
+                    <p className="text-xl font-bold">{item.name.common}</p>
+                    <p className="">
+                      {String(item.population).replace(
+                        /\B(?=(\d{3})+(?!\d))/g,
+                        ","
+                      )}
+                      <span className="text-xl"> people</span>
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         ))}
       </Carousel>
